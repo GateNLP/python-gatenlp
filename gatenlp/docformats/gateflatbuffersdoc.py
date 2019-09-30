bf95b202e51b3a070dc62e1b52f5cd5135622e05
Johann Petrak <johann.petrak@gmail.com>
2019-09-30 09:27:45 +0200

    WIP

---
+++ gatenlp/docformats/gateflatbuffersdoc.py
@@ -0,0 +1,4 @@
+"""
+For now, a placeholder to remind us looking into whether it makes sense to use flatbuffers as an additional
+storage/wire format. May be more compact and much faster than json or even bson.
+"""
