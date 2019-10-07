0b41afe6645643154c7a1f66a22b478a93441f75
Johann Petrak <johann.petrak@gmail.com>
2019-10-07 11:52:14 +0200

    WIP

--- gatenlp/gate_interaction.py
+++ gatenlp/gate_interaction.py
@@ -1,5 +1,10 @@
 """
-Support for interacting between a GATE (java) process and a gatenlp (Python) process
+Support for interacting between a GATE (java) process and a gatenlp (Python) process.
+Make this specifically work with gatelib-interaction!
+TODO: check if we can somehow do this: save the original stdout and 
+create our own handle, then for transmitting data, use that handle 
+so that any stdout from library calls does not interfere with the 
+data interchange??
 """
 
 # TODO
