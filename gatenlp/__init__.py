260ba01c21f5217de16fffc3e2622ee38c9a04dc
Johann Petrak <johann.petrak@gmail.com>
2020-01-17 16:32:42 +0100

    Do not import GateSlave by default because it needs py4j

--- gatenlp/__init__.py
+++ gatenlp/__init__.py
@@ -21,7 +21,7 @@ from gatenlp.document import Document
 from gatenlp.annotation_set import AnnotationSet
 from gatenlp.exceptions import InvalidOffsetException
 from gatenlp.changelog import ChangeLog
-from gatenlp.gateslave import GateSlave
+# from gatenlp.gateslave import GateSlave
 
 __all__ = ["GateNlpPr", "Annotation", "Document", "AnnotationSet",
            "InvalidOffsetException", "ChangeLog", "logger"]
