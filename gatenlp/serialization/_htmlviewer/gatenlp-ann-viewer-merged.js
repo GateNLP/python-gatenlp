a375b7b22cc18288642ed975f776ec26c43f436f
Johann Petrak <johann.petrak@gmail.com>
2020-10-15 10:22:28 +0200

    Update merged version of JS

--- gatenlp/serialization/_htmlviewer/gatenlp-ann-viewer-merged.js
+++ gatenlp/serialization/_htmlviewer/gatenlp-ann-viewer-merged.js
@@ -438,7 +438,7 @@ var gatenlpDocView = class {
     }
 
     htmlEntities(str) {
-        return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/\n/g, "<br>");
+        return str.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replace('"', '&quot;').replace("\n", '<br>');
     }
 };
 // console.log("Classes defined, defining gatenlp_run");
