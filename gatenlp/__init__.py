2bbc324047b8136086db0612df831f76fa955638
Johann Petrak <johann.petrak@gmail.com>
2020-10-27 12:43:46 +0100

    Bump version to 0.9.1

--- gatenlp/__init__.py
+++ gatenlp/__init__.py
@@ -1,6 +1,6 @@
 # NOTE: do not place a comment at the end of the version assignment
 # line since we parse that line in a shell script!
-__version__ = "0.9"
+__version__ = "0.9.1"
 import logging
 try:
     import sortedcontainers
