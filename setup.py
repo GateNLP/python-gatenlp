e2890a1705eb635b1ca2b0fc5f3bf9ef2dc66ccd
Johann Petrak <johann.petrak@gmail.com>
2021-01-30 21:32:00 +0100

    Add some dependencies

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ def get_install_extras_require():
             "jupyterlab",
             "notebook",
             "voila",
+            "RISE",
             "ipywidgets",
         ],
         "gazetteers": ["matchtext", "recordclass"],
