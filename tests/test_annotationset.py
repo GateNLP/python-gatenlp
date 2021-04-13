80fca92528dad718a7127d74853fa78e5140261c
Johann Petrak <johann.petrak@gmail.com>
2021-04-13 08:36:06 +0200

    Add unit test modules

--- tests/test_annotationset.py
+++ tests/test_annotationset.py
@@ -30,6 +30,53 @@ def make_doc():
     set1.add(36, 42, "Ann12")
     return doc
 
+class TestAnnotationSet01:
+
+    def test_annotationset01m01(self):
+        """
+        Unit test method (make linter happy)
+        """
+        from gatenlp.document import Document
+
+        txt = " ".join([f"word{i:02d}" for i in range(10)])
+        doc = Document(txt)
+        annset = doc.annset()
+        # create a Token annotation for each word
+        # create "At3_1" annotations for a single token whenever i is a multiple of 3
+        # create "At3_2" annotations for two tokens whenever i is a multiple of 3
+        for i in range(10):
+            annset.add(i * 7, i * 7 + 6, "Token", features={"i": i})
+            if i % 3 == 0:
+                annset.add(i * 7, i * 7 + 6, "At3_1", features={"i": i})
+                # cannot span two tokens at the very end
+                if i < 9:
+                    annset.add(i * 7, i * 7 + 6 + 7, "At3_2", features={"i": i})
+        # check: get all Token annotations
+        ret = annset.with_type("Token")
+        assert len(ret) == 10
+        # check get all At3_1 annotations
+        ret = annset.with_type("At3_1")
+        assert len(ret) == 4
+        ret = annset.with_type("At3_2")
+        assert len(ret) == 3
+        ret = annset.with_type("Token", "At3_1")
+        assert len(ret) == 14
+        ret = annset.with_type("At3_1", "Token")
+        assert len(ret) == 14
+        ret = annset.with_type("Token", "At3_1", non_overlapping=True)
+        # print(f"\n!!!!!!!!!!!!DEBUG: anns for Token/At3_1={ret}")
+        assert len(ret) == 10
+        ret = annset.with_type("Token", "At3_2", non_overlapping=True)
+        # print(f"\n!!!!!!!!!!!!DEBUG: anns for Token/At3_2={ret}")
+        assert len(ret) == 10
+        ret = annset.with_type("At3_1", "Token", non_overlapping=True)
+        # print(f"\n!!!!!!!!!!!!DEBUG: anns for At3_1/Token={ret}")
+        assert len(ret) == 10
+        ret = annset.with_type("At3_2", "Token", non_overlapping=True)
+        # print(f"\n!!!!!!!!!!!!DEBUG: anns for At3_2/Token={ret}")
+        assert len(ret) == 7
+        # TODO: check other kinds of overlap in the original set!
+
 
 class TestAnnotationSetRels:
 
---
+++ tests/test_offsetmapper.py
@@ -0,0 +1,39 @@
+import sys
+
+
+class TestOffsetMapper01:
+    def test_offsetmapper01m01(self):
+        """
+        Unit test method (make linter happy)
+        """
+        from gatenlp.document import OffsetMapper, Document
+
+        c_poo = "\U0001F4A9"
+        c_bridge = "\U0001f309"
+        doc1 = Document("01" + c_poo + "3" + c_bridge + c_bridge + c_bridge + "7")
+        assert len(doc1) == 8
+        assert doc1[2] == c_poo
+        om1 = OffsetMapper(doc1)
+        assert len(om1.java2python) == 13
+        p2j = [0, 1, 2, 4, 5, 7, 9, 11, 12]
+        # print("p2j={}".format(p2j), file=sys.stderr)
+        # print("om1.p2j={}".format(om1.python2java), file=sys.stderr)
+        assert om1.python2java == p2j
+        j2p = [0, 1, 2, 2, 3, 4, 4, 5, 5, 6, 6, 7, 8]
+        assert om1.java2python == j2p
+        for i in om1.java2python:
+            joff = om1.convert_to_java(i)
+            poff = om1.convert_to_python(joff)
+            assert poff == i
+
+    def test_offsetmapper01m02(self):
+        """
+        Unit test method (make linter happy)
+        """
+        # test identical offsets
+        from gatenlp.document import OffsetMapper, Document
+        doc1 = Document("Just some sample document")
+        om1 = OffsetMapper(doc1)
+        for idx in range(len(doc1)):
+            assert idx == om1.convert_to_java(idx)
+            assert idx == om1.convert_to_python(idx)
