0795bd8f11b46186c46e3e151ac983003f455919
Johann Petrak <johann.petrak@gmail.com>
2021-05-23 18:07:34 +0200

    Make PampacAnnotator available for import

--- gatenlp/pam/pampac/__init__.py
+++ gatenlp/pam/pampac/__init__.py
@@ -7,7 +7,7 @@ Provides a language and classes for patterns, actions, rules.
 from gatenlp.pam.pampac.data import Location, Failure, Success, Result, Context
 from gatenlp.pam.pampac.pampac_parsers import PampacParser, All, And, Ann, AnnAt, Call, Lookahead, Find
 from gatenlp.pam.pampac.pampac_parsers import Function, N, Or, Seq, Text
-from gatenlp.pam.pampac.pampac import Pampac
+from gatenlp.pam.pampac.pampac import Pampac, PampacAnnotator
 from gatenlp.pam.pampac.rule import Rule
 from gatenlp.pam.pampac.actions import AddAnn, Actions, UpdateAnnFeatures
 from gatenlp.pam.pampac.actions import Getter
