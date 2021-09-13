30611ab3216004496fabc9fbae7557606e6e7ac3
Joan Codina <Joan.codina@ensembleip.com>
2021-09-13 19:59:45 +0200

    Add test to parameters passing

--- tests/test_spacy.py
+++ tests/test_spacy.py
@@ -80,3 +80,81 @@ class TestSpacy01:
         tokens = annsOut.with_type("Token")
         assert len(tokens) == 7
 
+
+    def test_spacy03(self):
+        """
+        Unit test method to test data passing between spacy and gate
+        """
+        try:
+            import spacy
+            from spacy.language import Language
+            from spacy.tokens import Doc
+            from spacy.matcher import Matcher
+            from gatenlp.lib_spacy import spacy2gatenlp, apply_spacy
+            nlp = spacy.load("en_core_web_sm")
+        except ImportError:
+            logger.warning("Module spacy or model en_core_web_sm not installed, skipping spacy test")
+            return
+        
+        ## create a language factory
+        @Language.factory('number_detector' )
+        def create_number_detector_component(nlp: Language, name: str):
+            return NumberDetector(nlp)
+
+
+        class NumberDetector(object):
+
+            name = "number_detector"
+            matcher: Matcher
+            nlp: Language
+ 
+            def __init__(self, nlp: Language):
+                # Extensions  include an identifier of the component that creates it, to avoid collisions
+                Doc.set_extension("Number_freq",default=False)
+                self.nlp = nlp
+                self.matcher = Matcher(nlp.vocab)
+                pattern = [{'IS_DIGIT': True}]
+                
+                self.matcher.add("numbers", [pattern], greedy="LONGEST")
+
+            def __call__(self, doc: Doc,number:str,**kwargs) -> Doc:
+                # This method is invoked when the component is called on a Doc
+                doc._.Number_freq=number
+                matches = self.matcher(doc, with_alignments=True)
+                refSpans=[]
+                for match_id, start, end, matched_list in matches:
+                    refSpans.append(doc[start:end])
+                    # logging.debug('
+                    #     f" matched {self.nlp.vocab[match_id].text} -  { ' '.join( [t.text for t in doc[start:end]])}"
+                    #     f"  -- matching {matched_list} -- {ref_set}"
+                    # )
+                doc.spans["Numbers"]=refSpans
+                return doc
+        
+        
+        txt = "When 2 plus 2 makes 5 then  your system is doing something wrong!. But in life 2 and 2 not always makes 4. "
+        nlp.add_pipe('number_detector')
+        doc = Document(txt)
+        annset = doc.annset()
+        annset.add(0, 66, "Sentence")
+        annset.add(67, 106, "Sentence")
+        
+        anns = doc.annset()
+        containing_set1 = anns.with_type("Sentence")
+        assert len(containing_set1) == 2
+        tokens = anns.with_type("Token")
+        assert len(tokens) == 0 
+        i=1
+        for ann in containing_set1:
+            ann.features["number"]=i
+            i+=1    
+        gdoc = apply_spacy(nlp, doc, setname="spacy1", containing_anns=containing_set1,component_cfg="number_detector",retrieveSpans=["Numbers"])
+      
+        annsOut = gdoc.annset("spacy1")
+        sents = annsOut.with_type("Sentence")
+        assert len(sents) == 2
+        nums = annsOut.with_type("Numbers")
+        assert len(nums) == 6
+        for ann in containing_set1:
+            assert ann.features["number"]== ann.features["Number_freq"]
+        
