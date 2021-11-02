22122ff8ad026c6dd2ea2dfbe0fdbfade2ac1cba
Johann Petrak <johann.petrak@gmail.com>
2021-11-02 08:22:46 +0100

    Add outline for the feature gazetteer.

---
+++ gatenlp/processing/gazetteer/featuregazetteer.py
@@ -0,0 +1,44 @@
+"""
+Module that implements the FeatureGazetteer which matches the value of some feature of some
+annotation type against a string gazetteer and adds, removes or updates annotations if a
+match does or does not occur.
+"""
+from gatenlp.processing.annotator import Annotator
+
+
+class FeatureGazetteer(Annotator):
+    def __init__(self, stringgaz,
+                 ann_type,
+                 containing_type=None,
+                 feature="",
+                 annset_name="",
+                 outset_name="",
+                 out_type="Lookup",
+                 match_at_start_only=True,
+                 match_at_end_only=True,
+                 processing_mode="add-features",
+                 handle_multiple="first"
+                 ):
+        """
+        Create a feature gazetteer. This gazetteer processes all annotations of some type in some
+        input annotation set and tries to match the value of some feature against the given string
+        gazetteer. If a match is found, the action defined through the processing_mode parameter is
+        taken. If the annotation does not have the feature or no match occurs, no action is performed.
+
+        Args:
+            stringgaz:
+            ann_type:
+            containing_type:
+            feature:
+            annset_name:
+            outset_name:
+            out_type:
+            match_at_start_only:
+            match_at_end_only:
+            processing_mode:
+            handle_multiple:
+        """
+        pass
+
+    def __call__(doc, **kwargs):
+        pass
--- gatenlp/processing/matcher.py
+++ gatenlp/processing/matcher.py
@@ -30,20 +30,53 @@ class StringRegexAnnotator(Annotator):
                 to a dictionary of features to assign. If the feature value is the string "$n" with n a group
                 number, then the value for that match group is used.
                 Or the source_fmt can be "file" in which case the rules are loaded from a file with that path.
-            outset_name:
-            out_type:
-            annset_name:
-            containing_type:
-            features:
-            skip_longest:
-            match:
-            engine:
+            outset_name: name of the output annotation set
+            out_type: default type for output annotations, if the type is not specified with the rule
+            annset_name: the input annotation set if matching is restricted to spans covered by
+                containing annotations.
+            containing_type: if this is not None, then matching is restricted to spans covered by annotations
+                of this type. The annotations should not overlap.
+            list_features: a dictionary of arbitrary default features to add to all annotations created
+                for matches from any of the rules loaded from the current source
+            skip_longest: if True, after a match, the next match is attempted after the longest current
+                match. If False, the next match is attempted at the next offset after the start of the
+                current match.
+            match: the strategy of which rule to apply. One of: "all": apply all matching rules.
+                "first": apply the first matching rule, do not attempt any others. "firstlongest":
+                try all rules, apply the first of all rules with the longest match. "alllongest":
+                try all rules, apply all rules with the longest match.
+            engine: identifies which Python regular expression engine to use. Currently either
+                "re" or "regexp" to use the package with the corresponding name. Only the package
+                used is attempted to get loaded.
+        """
+        pass
+
+    def add(self, rule):
+        """
+        Add a single rule.
+
+        Args:
+            rule: a tuple where the first element is a compiled regular expression and the second
+                element is a tuple that describes the annotations to create if a match occurs.
+                The first element of that tuple is the annotation type or None
+                to use the out_type. The second element is a dictionary mapping each group number of the match
+                to a dictionary of features to assign. If the feature value is the string "$n" with n a group
+                number, then the value for that match group is used.
+        """
+        pass
+
+    def append(self, source, source_fmt="file", list_features=None):
+        """
+        Load a list of rules.
+
+        Args:
+            source: where/what to load. See the init parameter description.
+            source_fmt: the format of the source, see the init parameter description.
+            list_features: if not None a dictionary of features to assign to annotations created
+                for any of the rules loaded by that method call.
         """
         pass
 
     def __call__(self, doc: Document, **kwargs):
         pass
 
-# class AnnotationRegexMatcher:
-#    """ """
-#    pass
