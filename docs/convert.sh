a835a685b2c017f15569653c48d95616fb50f6a4
Johann Petrak <johann.petrak@gmail.com>
2022-06-29 08:05:17 +0200

    Convert some files to MD via HTML

--- docs/convert.sh
+++ docs/convert.sh
@@ -2,7 +2,7 @@
 
 # Note: the following files must get converted manually because they depend on 
 # authentication or cannot be fully automated for other reasons:
-# client_elg
+# client_elg: needs interactive authentication
 # gateworker: start the PythonWorkerLr in GATE gui on default port,
 #    run: gatenlp-gate-worker --port 31333 --auth even-more-secret
 toconvert=$( cat <<EOF
@@ -18,18 +18,30 @@ client_ibmnlu
 client_tagme
 client_textrazor
 client_googlenlp
-corpora
 processing
 tokenizers
 gazetteers
 stringregex
 pampac
+EOF
+)
+
+toconvert2=$( cat <<EOF
 visualization
+corpora
 EOF
 )
 
 for file in $toconvert
 do
-  echo Converting $file
+  echo Converting $file to Markdown
   jupyter nbconvert -y --execute --to markdown ${file}.ipynb
+done
+
+for file in $toconvert2
+do
+  echo Converting $file via HTML to Markdown
+  jupyter nbconvet -y --execute --to html ${file}.ipynb
+  tail +3 ${file}.html  | sed -e 's/<head>//' -e 's/<\/head>//' -e 's/<body>//' -e 's/<\/body>//' -e 's/<\/html>//' > ${file}.md
+  rm ${file}.html
 done
