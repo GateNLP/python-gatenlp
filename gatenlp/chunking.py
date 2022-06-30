06b69521f54c0b139991b06d5213ca7b6a33d949
Johann Petrak <johann.petrak@gmail.com>
2022-06-30 09:54:52 +0200

    WIP

---
+++ gatenlp/chunking.py
@@ -0,0 +1,110 @@
+"""
+Module for chunking-related methods and annotators.
+"""
+import re
+from typing import Union, List, Optional, Dict, Generator, Tuple
+import iobes
+from gatenlp import Document, Span, AnnotationSet
+
+SPANENCS = dict(
+    BIO=iobes.SpanEncoding.BIO,
+    IOB=iobes.SpanEncoding.IOB,
+    IOBES=iobes.SpanEncoding.IOBES,
+    BILOU=iobes.SpanEncoding.BILOU,
+    BMEOW=iobes.SpanEncoding.BMEOW,
+    BMEWO=iobes.SpanEncoding.BMEWO,
+)
+
+PAT_WS = re.compile(r"\s+")
+
+
+def normalize_type(typename):
+    """
+    Normalize the type name so it can be used as part of an BIO-like code that is usable in a conll-like dataset:
+    replace all whitespace with a hyphen.
+
+    Args:
+        typename: the chunk type name
+
+    Returns:
+        normalized chunk type name
+    """
+    return re.sub(PAT_WS, "-", typename)
+
+
+def doc_to_ibo(
+        doc: Document,
+        annset_name: str = "",
+        sentence_type: Optional[str] = None,
+        token_type: str = "Token",
+        token_feature: Optional[str] = None,
+        chunk_types: Optional[List[str]] = None,
+        type2code: Optional[Dict] = None,
+        scheme: str = "BIO",
+        return_rows = True,
+) -> Generator[Union[List, Tuple], None, None]:
+    """
+    Extract tokens and corresponding token entity codes.
+
+    Args:
+        doc: The document to process
+        annset_name: name of the annotation set which contains all the types needed
+        sentence_type: if None, use whole document, otherwise generate one result per sentence type annotation,
+            if the sentence contains at least one token.
+        token_type: type of token annotations to use
+        token_feature: if not None, use the feature instead of the covered document text
+        chunk_types: a list of annotation types which identify chunks, each chunk type is used as entity type
+            Note the chunk type annotations must not overlap, but this is currently not checked, for performance
+            reasons.
+        type2code: an optionam dict mapping the chunk_type to the type name used in the BIO codes
+        scheme: the encoding scheme to use, default is BIO, possible: IOB, BIO, IOBES, BILOU, BMEOW, BMEWO
+        return_rows: if True, return a list of (tokenstring, code) tuples for each sentence, if False return two lists
+            of equal length, the first with the token strings and the second with the codes
+
+    Yields:
+        either one list of (tokenstring, code) tuples per sentence found or two lists, one with the tokenstrings and
+        the other with the codes.
+    """
+    spanenc = SPANENCS[scheme]
+    if type2code is None:
+        type2code = {}
+    if sentence_type is None:
+        spans = [Span(0, len(doc))]
+    else:
+        spans = [a.span for a in doc.annset(annset_name).with_type(sentence_type)]
+    all_tokens = doc.annset(annset_name).with_type(token_type)
+    if chunk_types is None:
+        all_chunks = AnnotationSet()
+    else:
+        all_chunks = doc.annset(annset_name).with_type(chunk_types)
+    for span in spans:
+        tokens = all_tokens.within(span)
+        if len(tokens) == 0:
+            continue
+        # map token start offsets to token indices
+        start2idx = {t.start: idx for idx, t in enumerate(tokens)}
+        chunks = all_chunks.within(span)
+        # now we want to know which of all the tokens are covered by chunks. So for each chunk, we check
+        # which tokens are contained and append an iobes Span that points to the index of the token
+        iobes_spans = []
+        for chunk in chunks:
+            ctokens = list(tokens.within(chunk))
+            start = start2idx[ctokens[0].start]
+            end = start2idx[ctokens[-1].start]+1
+            iobes_span = iobes.Span(
+                type=type2code.get(chunk.type, normalize_type(chunk.type)),
+                start=start,
+                end=end,
+                tokens=tuple(range(start, end))
+            )
+            iobes_spans.append(iobes_span)
+        codes = iobes.write_tags(iobes_spans, spanenc, length=len(tokens))
+        assert len(tokens) == len(codes)
+        if token_feature:
+            token_strings = [t.features[token_feature] for t in tokens]
+        else:
+            token_strings = [doc[t] for t in tokens]
+        if return_rows:
+            yield [(t, c) for t, c in zip(token_strings, codes)]
+        else:
+            yield token_strings, codes
---
+++ gatenlp/corpora/export.py
@@ -0,0 +1,88 @@
+"""
+Module that defines DocumentDestination classes for exporting specific formats.
+"""
+import os
+from typing import Union, Optional, List, Dict, IO
+from gatenlp.corpora import DocumentDestination
+from gatenlp import Document
+from gatenlp.chunking import doc_to_ibo
+
+class Conll2003FileDestination(DocumentDestination):
+    """
+    Extracts tokens and BIO-like codes from the documents and writes those in CONLL (2003) format.
+    """
+
+    def __init__(
+            self,
+            file: Union[str, IO],
+            annset_name: str = "",
+            sentence_type: Optional[str] = None,
+            token_type: str = "Token",
+            token_feature: Optional[str] = None,
+            chunk_types: Optional[List[str]] = None,
+            type2code: Optional[Dict] = None,
+            scheme: str = "BIO",
+):
+        """
+        Create a Conll2003FileDestination to write CONLL 2003 format data to a file.
+
+        Args:
+            file: either the file path (str) or an open file handle for writing
+            annset_name: the annotation set which contains the annotation types needed
+            sentence_type: if None, use whole document, otherwise generate one result per sentence type annotation,
+                if the sentence contains at least one token.
+            token_type: type of token annotations to use
+            token_feature: if not None, use the feature instead of the covered document text
+            chunk_types: a list of annotation types which identify chunks, each chunk type is used as entity type
+                Note the chunk type annotations must not overlap, but this is currently not checked, for performance
+                reasons.
+            type2code: an optionam dict mapping the chunk_type to the type name used in the BIO codes
+            scheme: the encoding scheme to use, default is BIO, possible: IOB, BIO, IOBES, BILOU, BMEOW, BMEWO
+        """
+        super().__init__()
+        if isinstance(file, str):
+            self.fh = open(file, "wt", encoding="utf-8")
+        else:
+            self.fh = file
+        self.annset_name = annset_name
+        self.sentence_type = sentence_type
+        self.token_type = token_type
+        self.token_feature = token_feature
+        self.chunk_types = chunk_types
+        self.type2code = type2code
+        self.scheme = scheme
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, _extype, _value, _traceback):
+        self.fh.close()
+
+    def append(self, doc):
+        """
+        Append a document to the destination.
+
+        Args:
+            doc: the document, if None, no action is performed.
+        """
+        if doc is None:
+            return
+        assert isinstance(doc, Document)
+        for sentence_rows in doc_to_ibo(
+                annset_name=self.annset_name,
+                sentence_type=self.sentence_type,
+                token_type=self.token_type,
+                token_feature=self.token_feature,
+                chunk_types=self.chunk_types,
+                type2code=self.type2code,
+                scheme=self.scheme
+        ):
+            for token, code in sentence_rows:
+                print(token, code, sep="\t", file=self.fh)
+        self.fh.print(file=self.fh)  # empty line for sentence boundary
+        self._n += 1
+
+    def close(self):
+        self.fh.close()
+
+
